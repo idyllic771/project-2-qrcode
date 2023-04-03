3671797b0c1dc6c788bc5566ae29b73de338f668
idyllic771 <idyllicmary771@gmail.com>
2023-04-03 21:04:42 +0000

    Qrcode assignment submission

--- qrcodeApp.py
+++ qrcodeApp.py
@@ -1,62 +1,30 @@
-import os
-import tkinter as tk
-from tkinter import END, NW
-
-from PIL import Image, ImageTk
+import PySimpleGUI as sg
 import qrcode
 
-qr = qrcode.QRCode(
-    version=1,
-    error_correction=qrcode.constants.ERROR_CORRECT_L,
-    box_size=10,
-    border=4,
-)
-# create the main window
-root = tk.Tk()
+#sg.theme("GreenTan")
+#font = ('Monaco',18)
 
+qr_image= [sg.Image('',key ='-QRCODE-')]
 
-# set the window title
-root.title("QR Code Generator App")
+layout = [
+     [sg.Text('input a text to generate qr code',text_color='black',background_color='pink')],
+     [sg.Input('',key='-TEXT-')],
+     [sg.Button('create', key='-CREATE-',button_color=('white', 'green')), sg.Button('EXIT',button_color=('white', 'red'))],
+     [sg.Column([qr_image], justification='center')]
+             
+]
+ 
+window = sg.Window(" QR CODE GENERATOR", layout,background_color='pink')
 
-# create a label for the input box
-label_input = tk.Label(root, text="Enter your input:", )
-label_input.pack()
+while True:
+    event, values = window.read()
+    if event == "EXIT" or event == sg.WIN_CLOSED:
+        break
+    elif event =='-CREATE-':
+        text = values['-TEXT-']
+        if text:
+            img = qrcode.make(text)
+            img.save('qr_code.png')
+            window['-QRCODE-'].update('qr_code.png')
 
-# create the input box
-input_box = tk.Entry(root, width=50)
-input_box.pack()
-
-# create a frame for the buttons
-button_frame = tk.Frame(root)
-button_frame.pack(pady=10, padx=5)
-
-canvas = tk.Canvas(root, width=300, height=300)
-canvas.pack()
-label1 = tk.Label(image=None)
-
-
-# define a function to be called when the button is clicked
-
-def generateCode():
-    links = input_box.get()
-    print(links)
-    qr.add_data(links)
-    qr.make(fit=True)
-    img = qr.make_image(fill_color="black", back_color="white")
-    if not os.path.exists("images"):
-        os.makedirs("images")
-    img.save("images/qr_code.png")
-    global qr_image
-    qr_image = ImageTk.PhotoImage(file="images/qr_code.png")
-    canvas.create_image(0, 0, anchor=NW, image=qr_image)
-
-
-# create the generate qr code button
-button1 = tk.Button(button_frame, text="create", bg="green", width=20, command=generateCode)
-button1.pack(side=tk.LEFT)
-
-
-
-
-
-root.mainloop()
+window.close()
